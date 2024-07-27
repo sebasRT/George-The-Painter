39d044aeb91dcccf5688b25f275d476e2a4742a5
sebasRT <rtsebas11@gmail.com>
2024-07-27 15:29:58 -0400

    fix: :rotating_light: vercel compiler added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import icon from "astro-icon";
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.george-the-painter.com',
   integrations: [tailwind(), react(), icon({
     iconDir: "src/assets/icons"
   })],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
+  output: "hybrid",
+  adapter: vercel()
 });

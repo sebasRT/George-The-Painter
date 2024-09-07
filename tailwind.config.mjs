3dc58e29a7269b3247436f34a10973885fad6780
sebasRT <rtsebas11@gmail.com>
2024-09-07 11:26:15 -0400

    chore: Refactor HeroSection and update interior/exterior painting pages layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			minHeight: {
+				'view': 'calc(100svh-1rem)'
+			},
 			colors: {
 				'emerald': {
 			'50': '#f2fbf5',
